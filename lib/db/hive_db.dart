fb7e41027aa93321a63713446f6cdee32bc616f8
faisin111 <faisins16@gmail.com>
2025-12-16 23:41:33 +0530

    Initial Flutter project commit

--- lib/db/hive_db.dart
+++ lib/db/hive_db.dart
@@ -5,14 +5,16 @@ class HiveDB {
   static late Box users;
 
   static Future<void> init() async {
-    final dir = Directory('./my_server_data');
+    final baseDir = Directory.current.path;
+    final dir = Directory('$baseDir/my_server_data');
 
     if (!dir.existsSync()) {
       dir.createSync(recursive: true);
     }
 
     Hive.init(dir.path);
-
     users = await Hive.openBox('users');
+
+    print('Hive DB path: ${dir.absolute.path}');
   }
 }
