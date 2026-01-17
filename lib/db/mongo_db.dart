ef876d83a5c205e22cddec731afd2c8e82eb3665
faisin111 <faisins16@gmail.com>
2026-01-17 20:08:36 +0530

    Initial commit

--- lib/db/mongo_db.dart
+++ lib/db/mongo_db.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:mongo_dart/mongo_dart.dart';
 
 class MongoDb {
@@ -5,13 +7,16 @@ class MongoDb {
   static late DbCollection users;
 
   static Future<void> init() async {
+    final mongoUrl = Platform.environment['MONGO_URL'];
+
+    if (mongoUrl == null || mongoUrl.isEmpty) {
+      throw Exception('MONGO_URL is not set');
+    }
+    //  dbb=Db("mongodb+srv://faisin123:CLzyys6WZqXHvI1J@cluster0.h2wjcex.mongodb.net/?dartServer=Cluster0")
     // db = Db(
     //   "mongodb+srv://faisin123:CLzyys6WZqXHvI1J@cluster0.h2wjcex.mongodb.net/?dartServer=Cluster0",
     // );
-    db = Db(
-      "mongodb://faisin123:CLzyys6WZqXHvI1J@HOST1:27017,HOST2:27017,HOST3:27017/dartServer",
-      "?ssl=true&replicaSet=atlas-xxxxx&authSource=admin&retryWrites=true&w=majority",
-    );
+    db = Db(mongoUrl);
     await db.open();
     users = db.collection('users');
     print('Mongo DB created');
